63a471e5a053e52215070207f789b5ced8cde58e
Prajwal <prajwalramteke9@gmail.com>
2023-03-02 10:31:30 +0530

    last commit hide

--- src/Components/WeatherApp.js
+++ src/Components/WeatherApp.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Time from "./Time";
 
+const apiKey = process.env.REACT_APP_API_KEY;
+
 export default function WeatherApp() {
   const [city, setCity] = useState("Nagpur");
   const [weatherData, setWeatherData] = useState({});
@@ -12,7 +14,7 @@ export default function WeatherApp() {
     setError(null);
     fetchApi();
   };
-const handleChange = (event) => {
+  const handleChange = (event) => {
     setCity(event.target.value);
     // console.log(event.target.value);
   };
@@ -22,48 +24,75 @@ const handleChange = (event) => {
   }, []);
 
   const fetchApi = () => {
-    axios.get(
-        `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=7700d2842cebea400b6af27412b98602`
+    axios
+      .get(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`
       )
       .then((response) => {
         setWeatherData(response.data);
         // console.log(response.data);
       })
       .catch((error) => {
-        setError("Data Not Found");
+        // setError("Data Not Found");
+        // console.error(`get your API key first: ${error}`);
       });
   };
-  const handelEnter=()=>{
+  const handelEnter = () => {
     handleSubmit();
+  };
+
+  if (!apiKey) {
+    return <a href="">Required: Get your unsplash API key...</a>;
   }
 
   return (
     <div className="container">
       <form onSubmit={handleSubmit}>
-        <input type="text" value={city} onChange={handleChange} onKeyUp={handelEnter}/>
+        <input
+          type="text"
+          value={city}
+          onChange={handleChange}
+          onKeyUp={handelEnter}
+        />
         <button type="submit">Search</button>
       </form>
 
-      {error ? ( <h1>{error}</h1>):
-      ( weatherData.main && (
-        <div className="data">
-
-            <h2>{weatherData.sys.country}, {city}</h2>
-            <h3><Time/></h3>
+      {error ? (
+        <h1>{error}</h1>
+      ) : (
+        weatherData.main && (
+          <div className="data">
+            <h2>
+              {weatherData.sys.country}, {city}
+            </h2>
+            <h3>
+              <Time />
+            </h3>
             <div className="wicon">
-               <img  id="icon"  src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`} alt="weather icon"></img>
+              <img
+                id="icon"
+                src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`}
+                alt="weather icon"
+              ></img>
             </div>
-            <p className="temperature">{weatherData.main.temp}°<span>C</span></p>
+            <p className="temperature">
+              {weatherData.main.temp}°<span>C</span>
+            </p>
             <p className="description">{weatherData.weather[0].description}</p>
-            
+
             <div className="minmax">
-              <p>Min: {weatherData.main.temp_min}<span>°C</span></p>
-              <p>Max: {weatherData.main.temp_max}<span>°C</span></p>
+              <p>
+                Min: {weatherData.main.temp_min}
+                <span>°C</span>
+              </p>
+              <p>
+                Max: {weatherData.main.temp_max}
+                <span>°C</span>
+              </p>
             </div>
 
             <p>Humidity: {weatherData.main.humidity}</p>
-
-        </div>
+          </div>
         )
       )}
     </div>
